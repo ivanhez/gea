5792738babaf4e989fcd3c8cb399c00fae862a7e
ivanhez <ivanhez96@gmail.com>
2023-11-23 16:39:47 -0600

    build

---
+++ build/CMakeFiles/PONG.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/Desktop/pong/src/Game/Game.cpp" "CMakeFiles/PONG.dir/src/Game/Game.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Game/Game.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/Game/Graphics/Texture.cpp" "CMakeFiles/PONG.dir/src/Game/Graphics/Texture.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Game/Graphics/Texture.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/Game/Graphics/TextureManager.cpp" "CMakeFiles/PONG.dir/src/Game/Graphics/TextureManager.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Game/Graphics/TextureManager.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/Pong/Pong.cpp" "CMakeFiles/PONG.dir/src/Pong/Pong.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Pong/Pong.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/Pong/Systems.cpp" "CMakeFiles/PONG.dir/src/Pong/Systems.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Pong/Systems.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/Scene/Scene.cpp" "CMakeFiles/PONG.dir/src/Scene/Scene.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/Scene/Scene.cpp.obj.d"
+  "C:/Users/Admin/Desktop/pong/src/main.cpp" "CMakeFiles/PONG.dir/src/main.cpp.obj" "gcc" "CMakeFiles/PONG.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PONG.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PONG.dir/src/Game/Game.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Game/Game.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/Game/Graphics/Texture.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Game/Graphics/Texture.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/Game/Graphics/TextureManager.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Game/Graphics/TextureManager.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/Pong/Pong.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Pong/Pong.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/Pong/Systems.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Pong/Systems.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/Scene/Scene.cpp.obj"
+  "CMakeFiles/PONG.dir/src/Scene/Scene.cpp.obj.d"
+  "CMakeFiles/PONG.dir/src/main.cpp.obj"
+  "CMakeFiles/PONG.dir/src/main.cpp.obj.d"
+  "PONG.exe"
+  "PONG.exe.manifest"
+  "PONG.pdb"
+  "libPONG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PONG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
